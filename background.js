13270d58ffb6a08677c7185acb412bf255436d42
Zach Schneider <schneidmaster@gmail.com>
2015-08-07 15:21:39 -0400

    Fix up backup storage

--- background.js
+++ background.js
@@ -14,18 +14,29 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
   chrome.runtime.onMessage.addListener(
     function(signup) {
-      chrome.syncFileSystem.requestFileSystem(function(fs) {
-        var now = new Date();
-        var file = (now.getMonth() + 1) + "-" + (now.getDate()) + "-" + (now.getFullYear()) + "-signups.csv";
-        fs.root.getFile(file, {
-          create: true
-        }, function(f) {
-          f.createWriter(function(fileWriter) {
-            fileWriter.seek(fileWriter.length);
-            var signupStr = signup.name + "," + signup.email + "," + signup.phone + "," + signup.zip + "," + signup.canText;
-            fileWriter.write(new Blob([signupStr], {
-              type: 'text/plain'
-            }));
+      chrome.storage.local.get('generatedId', function(foundId) {
+        var id = null;
+        if(foundId.generatedId == null) {
+          id = parseInt(Math.random() * 10000000);
+          chrome.storage.local.set({'generatedId': id});
+        }
+        else {
+          id = foundId.generatedId;
+        }
+
+        chrome.syncFileSystem.requestFileSystem(function(fs) {
+          var now = new Date();
+          var file = (now.getMonth() + 1) + "-" + (now.getDate()) + "-" + (now.getFullYear()) + "-" + id + "-signups.csv";
+          fs.root.getFile(file, {
+            create: true
+          }, function(f) {
+            f.createWriter(function(fileWriter) {
+              fileWriter.seek(fileWriter.length);
+              var signupStr = "\n" + signup.first_name + "," + signup.last_name + "," + signup.email + "," + signup.phone + "," + signup.zip + "," + signup.canText;
+              fileWriter.write(new Blob([signupStr], {
+                type: 'text/plain'
+              }));
+            });
           });
         });
       });
